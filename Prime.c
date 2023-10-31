21a9bab6f7c848abf96ef87fb07d5e343f8b9438
KVaishnavi88 <147456940+KVaishnavi88@users.noreply.github.com>
2023-10-31 21:30:19 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         i;     }if(n%2!=0){             printf("Prime");     }else{         printf("Not Prime");     }
    }

---
+++ Prime.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        i;
+    }if(n%2!=0){
+            printf("Prime");
+    }else{
+        printf("Not Prime");
+    }
+    
+    
+}
