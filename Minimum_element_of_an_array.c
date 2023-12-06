677a3d9b7460f10d0dc8a2b95ac19aaa8dff9d28
KVaishnavi88 <147456940+KVaishnavi88@users.noreply.github.com>
2023-12-06 13:11:22 +0530

    #include<stdio.h> int main() {     int n,a; 	scanf("%d",&n); 	int arr[n]; 	for(int i=0;i<n;i++){ 		scanf("%d",&arr[i]); 		 a=arr[i];} 		for(int i=0;i<n;i++){ 		    if(a<=arr[i]) 		printf("%d",a); 		break; 	} 		//for(int i=0;i<n;i++){ 			//printf("%d ",dc); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,a;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+		 a=arr[i];}
+		for(int i=0;i<n;i++){
+		    if(a<=arr[i])
+		printf("%d",a);
+		break;
+	}
+		//for(int i=0;i<n;i++){
+			//printf("%d ",dc);
+}
